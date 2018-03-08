f4a05e8d83fb23e3d1a9e8fa247b097602582101
rfarney3 <farneyr@yahoo.com>
2018-03-08 16:34:18 -0500

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,7 @@ class PeopleController < ApplicationController
     @person = Person.new
   end
 
-  def create    
+  def create
     Person.create(person_params)
     redirect_to people_path
   end
@@ -16,5 +16,8 @@ class PeopleController < ApplicationController
 
   def person_params
     params.require(:person).permit(:name)
+  end
+
+  def get_person
   end
 end
